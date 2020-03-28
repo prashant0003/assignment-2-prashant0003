2dc9771bff15411e921c48e862ef2443542bc774
prashant0003 <61289135+prashant0003@users.noreply.github.com>
2020-03-28 19:13:09 +0530

    Update MyQueue.java

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -8,4 +8,18 @@ package problem4.myqueue;
 // to create queue to store pre - order successor
 
 public class MyQueue {
+    private int []arr;
+    private int front;
+
+    public MyQueue(int[] arr){
+        this.arr=arr;
+        this.front=0;
+    }
+
+    public int dequeue() {
+        if(front<arr.length){
+            return arr[front++];
+        }
+        return -1;
+    }
 }
