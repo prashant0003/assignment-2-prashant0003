560863d69f0dcb67cbf9e49d5c3a76b38a39aea6
prashant0003 <61289135+prashant0003@users.noreply.github.com>
2020-03-28 19:02:08 +0530

    main function sucessfully implemented

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -8,4 +8,36 @@ package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+  public static void main(String []args) {
+        MyBinarySearchTree tree=new MyBinarySearchTree();
+        tree.insert(10);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(50);
+        tree.insert(40);
+        tree.insert(0);
+        tree.insert(25);
+        tree.insert(5);
+        tree.insert(4);
+        //tree.insert(8);
+        int [] postOrderTraversal=tree.postOrderTraversal();
+        int [] preOrderTraversal=tree.preOrderTraversal();
+        if(postOrderTraversal[postOrderTraversal.length-1]==preOrderTraversal[0] && preOrderTraversal[0]==tree.getRoot().getData()) {
+            System.out.println("yes in pre order traversal root element is the first element and in post order traversal root element is the last element.");
+        }
+        else {
+            System.out.println("no in pre order traversal root element is the first element and in post order traversal root element is the last element.");
+        }
+        if(postOrderTraversal.length%2!=0) {
+            if(postOrderTraversal[postOrderTraversal.length/2]==preOrderTraversal[preOrderTraversal.length/2]) {
+                System.out.println("preOrderTraversal and postOrderTraversal have same mid point");
+            }
+            else {
+                System.out.println("preOrderTraversal and postOrderTraversal do not have same mid point");
+            }
+        }
+        else{
+            System.out.println("elements in a tree is even.");
+        }
+    }
 }
