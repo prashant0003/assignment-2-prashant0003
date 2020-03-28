ca6b92c8890066de7336d5ada936d40878753a09
prashant0003 <61289135+prashant0003@users.noreply.github.com>
2020-03-28 18:58:30 +0530

    MyBinarySearchTree class completed
    postOrderTraversal
    preOrderTraversal
    and 2 more function added

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -14,7 +14,7 @@ public class MyBinarySearchTree {
 
     public TreeNode getRoot() {
         return root;
-    
+    }
     public boolean insert(int data) {
         if(root==null) {root=new TreeNode(data); length++; return true;}
         TreeNode currentNode=root;
@@ -26,5 +26,69 @@ public class MyBinarySearchTree {
             if(currentNode.getData()>data && currentNode.getLeft()!=null) {currentNode = currentNode.getLeft(); }
         }
     }
+    public int [] preOrderTraversal() {
+        arr=new int[length];
+        index=0;
+        return preOrderTraversal(root);
+    }
+
+    public int [] postOrderTraversal() {
+        arr=new int[length];
+        index=0;
+        return postOrderTraversal(root);
+    }
+
+    public int countNodeNotHavingLeftChild() { return countNodeNotHavingLeftChild(root);}
+
+    public void printAllLeftChild() { printAllLeftChild(root);}
+
+    private int[] preOrderTraversal(TreeNode currentNode) {
+        if(length==0){ return null; }
+        arr[index++]= currentNode.getData();
+        if(currentNode.getLeft()!=null){ preOrderTraversal(currentNode.getLeft()); }
+        if(currentNode.getRight()!=null) { preOrderTraversal(currentNode.getRight()); }
+        return arr;
+   }
+
+    private int[] postOrderTraversal(TreeNode currentNode) {
+        if(length==0){ return null; }
+        if(currentNode.getLeft()!=null){ postOrderTraversal(currentNode.getLeft()); }
+        if(currentNode.getRight()!=null) { postOrderTraversal(currentNode.getRight()); }
+        arr[index++]= currentNode.getData();
+        return arr;
+    }
+
+    private int countNodeNotHavingLeftChild(TreeNode node) {
+        if (length!=0) {
+            int temp = 0;
+            if (node.getLeft() != null && node.getRight() != null) {
+                temp += countNodeNotHavingLeftChild(node.getLeft());
+                temp += countNodeNotHavingLeftChild(node.getRight());
+            } else if (node.getLeft() != null && node.getRight() == null) {
+                temp += countNodeNotHavingLeftChild(node.getLeft());
+            } else if (node.getLeft() == null && node.getRight() != null) {
+                temp = 1;
+                temp += countNodeNotHavingLeftChild(node.getRight());
+            } else {
+                temp = 1;
+            }
+
+            return temp;
+        }
+        else{
+            return 0;
+        }
+    }
+
+    private void printAllLeftChild(TreeNode node) {
+        if(length!=0) {
+            if(node.getLeft()!=null) {
+                System.out.println(node.getLeft().getData());
+                printAllLeftChild(node.getLeft());
+            }
+            if(node.getRight()!=null) {
+                printAllLeftChild(node.getRight());
+            }
+        }
     }
 }
