811274b6926dc5a3441ab010f10f0c68ef6ea12f
prashant0003 <61289135+prashant0003@users.noreply.github.com>
2020-03-28 18:55:56 +0530

    insert fucntion completed

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -14,5 +14,17 @@ public class MyBinarySearchTree {
 
     public TreeNode getRoot() {
         return root;
+    
+    public boolean insert(int data) {
+        if(root==null) {root=new TreeNode(data); length++; return true;}
+        TreeNode currentNode=root;
+        while (true) {
+            if(currentNode.getData()==data){return false;}
+            if(currentNode.getData()<data && currentNode.getRight()==null) {currentNode.setRight(new TreeNode(data)); length++; return true;}
+            if(currentNode.getData()>data && currentNode.getLeft()==null) {currentNode.setLeft(new TreeNode(data)); length++; return true;}
+            if(currentNode.getData()<data && currentNode.getRight()!=null)  {currentNode = currentNode.getRight();}
+            if(currentNode.getData()>data && currentNode.getLeft()!=null) {currentNode = currentNode.getLeft(); }
+        }
+    }
     }
 }
