cb67856b7298f64872d4299d19582d0b65246e13
prashant0003 <61289135+prashant0003@users.noreply.github.com>
2020-03-28 19:18:20 +0530

    Update MyCircularQueue.java

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,55 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.student.Student;
+
 //to implement circular queue
 public class MyCircularQueue {
+    private Student [] arr;
+    private int front;
+    private int rare;
+
+    public MyCircularQueue(int length) {
+        this.arr = new Student[length];
+        this.front=this.rare=-1;
+    }
+
+    public boolean enqueue(Student data) {
+        if(data.getBackLog()==0) {
+            return false;
+        }
+        else if(front==-1){
+            front=rare=0;
+            arr[rare]=data;
+        }
+        else {
+            rare=(rare+1)%arr.length;
+            if(rare==front){
+                System.out.println("queue is full");
+                return false;
+            }
+            arr[rare]=data;
+        }
+        return true;
+    }
+
+    public String dequeue() {
+        if(front==-1) {
+            return "queue is empty";
+        }
+        else if(front==rare) {
+            String s=arr[front].toString();
+            front=rare=-1;
+            return s;
+        }
+        else {
+            return arr[front++].toString();
+        }
+    }
+
+    public String process() {
+        String s = arr[front].toString() + "\nBackLog-Appearing is " +(arr[front].getBackLog()-arr[front].getAppearingCount() );
+        return s;
+    }
 }
