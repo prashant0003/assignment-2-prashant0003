a5aa103c62ffae999f09cd2d0aaac88c58e65e1b
prashant0003 <61289135+prashant0003@users.noreply.github.com>
2020-03-28 19:15:19 +0530

    Update Student.java

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,52 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    private String name;
+    private int rollNumber;
+    private char section;
+    private String university;
+    private int appearingCount;
+    private int backLog;
+
+    public Student(String name, int rollNumber, char section, String university) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.section = section;
+        this.university = university;
+    }
+
+    public Student(String name, int rollNumber, char section, String university, int appearingCount, int backLog) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.section = section;
+        this.university = university;
+        if(appearingCount>2 ){  this.appearingCount = 2;
+            System.out.println("appearing count should not be greater than 2 hence default value 2 is selected "); }
+        else { this.appearingCount = appearingCount; }
+        this.backLog = backLog;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public int getAppearingCount() {
+        return appearingCount;
+    }
+
+    public int getBackLog() {
+        return backLog;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollNumber=" + rollNumber +
+                ", section=" + section +
+                ", university='" + university + '\'' +
+                ", appearingCount=" + appearingCount +
+                ", backLog=" + backLog +
+                '}';
+    }
 }
