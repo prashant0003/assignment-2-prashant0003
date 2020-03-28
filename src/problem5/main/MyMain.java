35aced93a19b6d2d46092a9303089614f6a9d325
prashant0003 <61289135+prashant0003@users.noreply.github.com>
2020-03-28 19:17:27 +0530

    Update MyMain.java

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,25 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        MyCircularQueue queue=new MyCircularQueue(5);
+        queue.enqueue(new Student("alpha",4,'C',"GLA",1,2));
+        queue.enqueue(new Student("beta",2,'C',"GLA",0,2));
+        queue.enqueue(new Student("gama",5,'C',"GLA",2,2));
+        queue.enqueue(new Student("delta",6,'C',"GLA",1,1));
+        System.out.println(queue.process());
+        queue.dequeue();
+        System.out.println(queue.process());
+        queue.dequeue();
+        queue.enqueue(new Student("alpha",4,'C',"GLA",1,2));
+        queue.enqueue(new Student("alpha",4,'C',"GLA",1,2));
+        queue.enqueue(new Student("alpha",4,'C',"GLA",1,2));
+        queue.enqueue(new Student("alpha",4,'C',"GLA",1,2));
+    }
 }
