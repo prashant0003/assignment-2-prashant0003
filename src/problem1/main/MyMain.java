7b1ef37b9da806ca7b542cad487fea3aef44e0f2
prashant0003 <61289135+prashant0003@users.noreply.github.com>
2020-03-28 19:00:28 +0530

    MyMain class implemented

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,4 +8,19 @@ package problem1.main;
 // executable class
 
 public class MyMain {
+   public static void main(String []args) {
+        MyBinarySearchTree tree=new MyBinarySearchTree();
+        tree.insert(10);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(50);
+        tree.insert(40);
+        tree.insert(0);
+        tree.insert(25);
+        tree.insert(5);
+        tree.insert(4);
+        tree.insert(8);
+        System.out.println("number of node not having left child: "+tree.countNodeNotHavingLeftChild()+"\nAll nodes which is left child of tree");
+        tree.printAllLeftChild();
+    }
 }
