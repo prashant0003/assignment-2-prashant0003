71581bcd420c371f30f0227e6f7b8346505b4676
prashant0003 <61289135+prashant0003@users.noreply.github.com>
2020-03-28 19:07:20 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,6 +7,8 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
    public static void main(String []args) {
         MyBinarySearchTree tree=new MyBinarySearchTree();
